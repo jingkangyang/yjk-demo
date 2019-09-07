3e6e66ea8bff8281ab25d10342a6f75df1e5d351
admin <admin@DESKTOP-0DLQ588>
2019-09-08 00:23:17 +0800

    第一次提交

---
+++ demo-thread/src/main/java/com/yjk/prj/demo/thread/ThreadDaemon.java
@@ -0,0 +1,41 @@
+package com.yjk.prj.demo.thread;
+
+/**守护线程
+ * @author yangjingkang
+ * 2019年9月8日 上午12:12:45
+ */
+public class ThreadDaemon extends Thread{
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("守护线程："+i);
+		}
+	}
+	
+	public static void main(String[] args) {
+		ThreadDaemon th = new ThreadDaemon();
+		th.setDaemon(true);
+		th.start();
+     
+		Thread th1 = new Thread(new Runnable() {
+			public void run() {
+				for (int i = 0; i < 5; i++) {
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("用户线程："+i);
+				}
+			}
+		});
+		th1.start();
+	}
+
+}
